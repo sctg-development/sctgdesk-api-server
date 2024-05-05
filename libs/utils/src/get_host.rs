84d3220d3136542ae348f5e78200eb1b815e38e3
Ronan <ronan.le_meillat@highcanfly.club>
2024-05-05 20:40:17 +0200

    refactor get_host

--- libs/utils/src/get_host.rs
+++ libs/utils/src/get_host.rs
@@ -1,12 +1,22 @@
 use std::collections::HashMap;
 
 pub fn get_host(headers: HashMap<String, String>) -> String {
-    if let Some(host) = headers.get("host") {
-        return format!("http://{}", host.to_string());
+    // Default to http
+    let mut proto = "http".to_string(); 
+
+    // Check if the headers contain the X-Forwarded-Proto header
+    if let Some(proto_in_headers) = headers.get("X-Forwarded-Proto") {
+        proto = proto_in_headers.to_string();
     }
 
-    if let (Some(proto), Some(host)) = (headers.get("X-Forwarded-Proto"), headers.get("X-Forwarded-Host")) {
-        return format!("{}://{}", proto, host);
+    // Check if the headers contain the X-Forwarded-Host header
+    if let Some(host_in_headers) = headers.get("X-Forwarded-Host") {
+        return format!("{}://{}", proto, host_in_headers);
+    }
+
+    // Default to the host header
+    if let Some(host) = headers.get("host") {
+        return format!("{}://{}",proto, host.to_string());
     }
 
     "".to_string()
