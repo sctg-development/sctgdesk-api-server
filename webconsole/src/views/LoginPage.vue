ec23e61a327d9bb9d3e0cffd3d18b124e0e8d894
Ronan <ronan.le_meillat@highcanfly.club>
2024-05-18 07:52:08 +0200

    required does not need ""

--- webconsole/src/views/LoginPage.vue
+++ webconsole/src/views/LoginPage.vue
@@ -10,7 +10,7 @@
                 <div>
                     <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Username</label>
                     <div class="mt-2">
-                        <input v-model="name" id="email" name="email" type="text" required=""
+                        <input v-model="name" id="email" name="email" type="text" required />
                             class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                     </div>
                 </div>
@@ -21,7 +21,7 @@
                     </div>
                     <div class="mt-2">
                         <input v-model="password" id="password" name="password" type="password"
-                            autocomplete="current-password" required=""
+                            autocomplete="current-password" required
                             class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                     </div>
                 </div>
