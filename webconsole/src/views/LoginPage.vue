b4640fb0ea2cb1a114a99a7c551b4141225e02c5
Ronan <ronan.le_meillat@highcanfly.club>
2024-05-18 07:41:22 +0200

    higher button

--- webconsole/src/views/LoginPage.vue
+++ webconsole/src/views/LoginPage.vue
@@ -28,7 +28,7 @@
 
                 <div>
                     <button type="submit"
-                        class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign
+                        class="flex w-full h-3 items-center justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign
                         in</button>
                 </div>
             </form>
